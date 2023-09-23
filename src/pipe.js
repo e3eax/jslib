c0cdd91548be247fe13a902dfc5fcd63c5908a6b
troublemanfx <143964705+troublemanfx@users.noreply.github.com>
2023-09-23 23:27:24 +0200

    deep clone the value passed to reduce

--- src/pipe.js
+++ src/pipe.js
@@ -1,10 +1,11 @@
+import clone from "./clone.js";
 import reduce from "./reduce.js";
 
 function pipe(...fns) {
   return function (val) {
 
     return reduce(fns, (prev, fn) => {
-      return fn.call(this, prev);
+      return fn.call(this, clone(prev));
     }, val);
 
   };
