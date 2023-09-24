d4c0f8a9feb380c6d3601038ce3f79246edbf787
troublemanfx <chadha.ansh@proton.me>
2023-09-24 02:48:41 +0200

    added memoize and modified concat

--- src/concat.js
+++ src/concat.js
@@ -1,7 +1,7 @@
 import curry from "./curry.js";
 
 var concat = curry(function concat(str, str2) {
-  return str + str2;
+  return str.toString() + str2.toString();
 });
 
 export default concat;
---
+++ src/memoize.js
@@ -0,0 +1,14 @@
+import concat from "./concat.js";
+
+function memoize(fn) {
+  var cache = {};
+
+  return function memoized(...args) {
+    const key = concat(args[0], args[1]);
+    if (cache.hasOwnProperty(key)) {
+      return cache[key];
+    }
+    cache[key] = fn(...args);
+    return cache[key];
+  };
+}
