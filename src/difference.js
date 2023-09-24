0b3bae5e34aae49f6d3638c3e7dee04d4ca1c74e
troublemanfx <chadha.ansh@proton.me>
2023-09-24 05:34:11 +0200

    difference works with objects now

--- src/difference.js
+++ src/difference.js
@@ -4,6 +4,10 @@ function difference(list, compare) {
   for (const x of list) {
     var includes = false;
     for (const y of compare) {
+      if (JSON.stringify(x) == JSON.stringify(y)) {
+        includes = true;
+        continue;
+      }
       if (x == y) includes = true;
     }
     if (!includes) notIncluded.push(x);
